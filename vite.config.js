86d66df778b0642b4fe21031abefcc99a07c3764
AlmarioKevin <kevinalmario15@gmail.com>
2025-10-10 19:53:15 -0500

    Configuración para el despliegue en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ApiSimpsons_2025IIg1_Almario_Kevin_and_Lopez_Mauricio/',
   server: {
     host: true, 
     port: 5173 
